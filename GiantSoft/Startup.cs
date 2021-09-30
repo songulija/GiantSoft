bae4dde7244549e1e04ec283bf856468a36fc157
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 14:02:01 +0300

    Configured Cors policy. To allow everybody to access API
    //cors, when someone thats not in your network tries accesing api,
                //by default your api will reject that request. so we want to configure this policy
                //for AddCors we need to add policy so it knows how to behave
                //so bellow with adding policys we can determine who is allowed to access our recources
                //if everybody on internet will use API you cant be to strict. In our case we allow everybody

--- GiantSoft/Startup.cs
+++ GiantSoft/Startup.cs
@@ -26,8 +26,20 @@ namespace GiantSoft
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            //cors, when someone thats not in your network tries accesing api,
+            //by default your api will reject that request. so we want to configure this policy
+            //for AddCors we need to add policy so it knows how to behave
+            //so bellow with adding policys we can determine who is allowed to access our recources
+            //if everybody on internet will use API you cant be to strict. In our case we allow everybody
+            services.AddCors(o =>
+            {
+                o.AddPolicy("AllowAll", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GiantSoft", Version = "v1" });
@@ -40,11 +52,16 @@ namespace GiantSoft
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GiantSoft v1"));
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GiantSoft v1"));
+
             app.UseHttpsRedirection();
+
+            //letting app know that it should use cors policy. We put policy name
+            //that we created for it to use
+            app.UseCors("AllowAll");
 
             app.UseRouting();
 
