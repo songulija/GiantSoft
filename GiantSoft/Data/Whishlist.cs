d8a3e2a5bc13f63e76ef0fa5a769c7d645152b93
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 13:41:23 +0300

    Added Whishlist data model which represents Whishlists table

--- GiantSoft/Data/Whishlist.cs
+++ GiantSoft/Data/Whishlist.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GiantSoft.Data
 {
+    /// <summary>
+    /// Whishlist table will have two Foreign Keys.
+    /// UserId and ProductId. Also has date when record was added
+    /// </summary>
     public class Whishlist
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey(nameof(ApiUser))]
+        public int UserId { get; set; }
+        [NotMapped]
+        public ApiUser ApiUser { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        [NotMapped]
+        public Product Product { get; set; }
+        public DateTime Date { get; set; }
+
     }
 }
