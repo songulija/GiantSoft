d3b9302e243c38b653a247b9f91a78d4c6d5836a
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 12:54:57 +0300

    Added Image data model which represents Images table from database

--- GiantSoft/Data/Image.cs
+++ GiantSoft/Data/Image.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GiantSoft.Data
 {
+    /// <summary>
+    /// Product can have many images. Relationship one to many
+    /// Have to add ForeignKey ProductId. Bellow define model that 
+    /// key comes from.
+    /// </summary>
     public class Image
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        [NotMapped]
+        public Product Product { get; set; }
+        public string Picture { get; set; }
     }
 }
