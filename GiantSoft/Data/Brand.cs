f7dc7375b6588982ac3dbedc27f0ecf83bd17a9e
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 12:29:56 +0300

    Added Brand model for Brands table in db. Added all other data models too.
    But other data models are empty for now

---
+++ GiantSoft/Data/Brand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiantSoft.Data
+{
+    /// <summary>
+    /// If its included you can get list of Products based on category
+    /// we dont need to include this into migration. so we use VIRTUAL
+    /// </summary>
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string BrandName { get; set; }
+
+        public virtual IList<Product> Products { get; set; }
+    }
+}
