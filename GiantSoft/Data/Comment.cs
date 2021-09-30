27e1523645c7d38803294ba13c7216dc3837dcf7
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 12:51:23 +0300

    Added Comment data model which represents Comments table

---
+++ GiantSoft/Data/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiantSoft.Data
+{
+    /// <summary>
+    /// Comment represents comments table in database.
+    /// It has two foreignKeys : ProductId and UserId.
+    /// Bellow have to define model Foreign key comes from[
+    /// use NotMapped so it will not affect database
+    /// </summary>
+    public class Comment
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        [NotMapped]
+        public Product Product { get; set; }
+        [ForeignKey(nameof(ApiUser))]
+        public int UserId { get; set; }
+        [NotMapped]
+        public ApiUser ApiUser { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public float Rating { get; set; }
+    }
+}
