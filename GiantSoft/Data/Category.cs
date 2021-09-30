7eccfffc96bd6d6c674777fa2aabb363a075859c
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 12:41:44 +0300

    Added Category data model which represents Categories table

--- GiantSoft/Data/Category.cs
+++ GiantSoft/Data/Category.cs
@@ -7,23 +7,20 @@ using System.Threading.Tasks;
 
 namespace GiantSoft.Data
 {
+    /// <summary>
+    /// Each category will have ParentId of parent category 
+    /// unless category is first then parentiD is null
+    /// If its included you can get list of Products based on category
+    /// we dont need to include this into migration. so we use VIRTUAL
+    /// </summary>
     public class Category
     {
-        /// <summary>
-        /// Each category will have ParentId of parent category 
-        /// unless category is first then parentiD is null
-        /// If its included you can get list of Products based on category
-        /// we dont need to include this into migration. so we use VIRTUAL
-        /// </summary>
-        public class Category
-        {
-            [Key]
-            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public int? ParentId { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
 
-            public virtual IList<Product> Products { get; set; }
-        }
+        public virtual IList<Product> Products { get; set; }
     }
 }
