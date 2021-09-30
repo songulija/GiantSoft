ddc7a2e1d7bdb74fc8ed3c1c90384fa0c9a5cf35
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 12:51:52 +0300

    Added Feedback data model which represents Feedbacks table

--- GiantSoft/Data/Feedback.cs
+++ GiantSoft/Data/Feedback.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GiantSoft.Data
 {
+    /// <summary>
+    /// Feedback table will have to Foreign Keys
+    /// from same table. ReceiverId and SenderId from ApiUsers table
+    /// </summary>
     public class Feedback
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey(nameof(ApiUser))]
+        public int ReceiverId { get; set; }
+        public int SenderId { get; set; }
+        [NotMapped]
+        public ApiUser ApiUser { get; set; }
+        public string FeedbackText { get; set; }
+        public double Rating { get; set; }
+        public DateTime Date { get; set; }
     }
 }
