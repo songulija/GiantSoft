e295051af978b01d9b02cdef2b1c8fa64cd0b237
songulija <65647664+songulija@users.noreply.github.com>
2021-09-30 13:00:10 +0300

    Added Payment data model which represents Payments table

--- GiantSoft/Data/Payment.cs
+++ GiantSoft/Data/Payment.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GiantSoft.Data
 {
+    /// <summary>
+    /// Payment class will have fields as status,updatetime,emailadress
+    /// that will come from paypal, when you make payment. ForeignKey is productId
+    /// </summary>
     public class Payment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        [NotMapped]
+        public Product Product { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; }
+        public string UpdateTime { get; set; }
+        public string EmailAdress { get; set; }
     }
 }
