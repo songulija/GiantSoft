5729f16173d6fd1e478719c61988f7d83386c705
songulija <65647664+songulija@users.noreply.github.com>
2021-10-09 17:39:14 +0300

    Added CreateBrand,UpdateBrand,DeleteBrand

--- GiantSoft/Controllers/BrandsController.cs
+++ GiantSoft/Controllers/BrandsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using GiantSoft.Data;
 using GiantSoft.IRepository;
 using GiantSoft.ModelsDTO;
 using Microsoft.AspNetCore.Authorization;
@@ -49,7 +50,7 @@ namespace GiantSoft.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [Authorize]
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}", Name = "GetBrand")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetBrand(int id)
@@ -57,6 +58,89 @@ namespace GiantSoft.Controllers
             var brand = await _unitOfWork.Brands.Get(b => b.Id == id);
             var result = _mapper.Map<BrandDTO>(brand);
             return Ok(result);
+        }
+
+
+        /// <summary>
+        /// POST request to api/brands route. Provide brandDTO in body
+        /// Convert CreateBrandDTO to domain
+        /// object Brand for database and create new brand record and save it
+        /// </summary>
+        /// <param name="brandDTO"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreateBrand([FromBody]CreateBrandDTO brandDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError($"Invalid CREATE attempt in {nameof(CreateBrand)}");
+                return BadRequest(ModelState);
+            }
+            var brand = _mapper.Map<Brand>(brandDTO);
+            await _unitOfWork.Brands.Insert(brand);
+            await _unitOfWork.Save();
+
+            return CreatedAtRoute("GetBrand", new { id = brand.Id }, brand);
+        }
+
+        /// <summary>
+        /// PUT request to api/brands route. Provide id, CreateBrandDTO in body. Check if brand with that id exist
+        /// Convert dto to domain object Brand and update
+        /// </summary>
+        /// <param name="brandDTO"></param>
+        /// <returns></returns>
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> UpdateBrand([FromBody] CreateBrandDTO brandDTO, int id)
+        {
+            //check if model is valid by CreateBrandDTO requirements
+            if (!ModelState.IsValid || id < 1)
+            {
+                _logger.LogError($"Invalid UPDATE attempt in {nameof(UpdateBrand)}");
+                return BadRequest(ModelState);
+            }
+            var brand = await _unitOfWork.Brands.Get(b => b.Id == id);
+            if (brand == null)
+            {
+                _logger.LogError($"Invalid UPDATE attempt in {nameof(UpdateBrand)}");
+                return BadRequest("Submited data is invalid");
+            }
+            //map/convert brandDTO into brand object. basically whatever is in brandDTO put it into brand
+            _mapper.Map(brandDTO, brand);
+
+            _unitOfWork.Brands.Update(brand);
+            await _unitOfWork.Save();
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// DELETE request to api/brands/{id} route. Provide id. Find record by
+        /// that id. Then delete it and save
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            var brand = await _unitOfWork.Brands.Get(b => b.Id == id);
+            if (brand == null)
+            {
+                _logger.LogError($"Invalid UPDATE attempt in {nameof(DeleteBrand)}");
+                return BadRequest("Submited data is invalid");
+            }
+            await _unitOfWork.Brands.Delete(id);
+            await _unitOfWork.Save();
+
+            return NoContent();
         }
     }
     
--- GiantSoft/Startup.cs
+++ GiantSoft/Startup.cs
@@ -70,7 +70,9 @@ namespace GiantSoft
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GiantSoft", Version = "v1" });
             });
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(op =>
+                op.SerializerSettings.ReferenceLoopHandling =
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
