4011741396b1cd9bad041c5639cec539166d6f32
songulija <65647664+songulija@users.noreply.github.com>
2021-10-09 15:55:34 +0300

    Added ConfgureJWT to ServiceExtensions

--- GiantSoft/ServiceExtensions.cs
+++ GiantSoft/ServiceExtensions.cs
@@ -1,9 +1,13 @@
 ﻿using GiantSoft.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GiantSoft
@@ -23,5 +27,39 @@ namespace GiantSoft
             //passing DatabaseContext that we are using as our database and AddDefaultToken
             builder.AddEntityFrameworkStores<DatabaseContext>().AddDefaultTokenProviders();
         }
+
+        //configuration for JWT in Startup. we also need IConfiguration
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration Configuration)
+        {
+            //gettings "Jwt" section from appsettings.json
+            var jwtSettings = Configuration.GetSection("Jwt");
+            //getting key that i set with Command line
+            var key = Environment.GetEnvironmentVariable("KEY");
+
+            //basically adding authentication to app. and default scheme that i want is JWT
+            //when somebody tries to authenticate check for bearer token
+            //THEN i set up parameters. ValidateIssuer means we want to validate token, validate lifetime and issuer key
+            //then we set ValidIssuer for any JWT token will be string from appsettings.json 
+            //then goes key that we hash. most important thing dont put KEY in appsettings
+            //based on your situation you may need more validation
+            //VALIDATE AUDIENCE TOO. to validate users
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.GetSection("Issuer").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+                };
+            });
+        }
+
     }
 }
