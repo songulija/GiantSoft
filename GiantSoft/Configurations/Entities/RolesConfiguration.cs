6bfe1b99629b25c53de5102ed88452ac2bc4ca25
songulija <65647664+songulija@users.noreply.github.com>
2021-10-09 11:47:19 +0300

    Added RolesConfiguration
    Added USER and ADMINISTRATOR roles to userRoles table

---
+++ GiantSoft/Configurations/Entities/RolesConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiantSoft.Configurations.Entities
+{
+    /// <summary>
+    /// Adding IdentityRoles. So each user will have role. Either admin or user
+    /// Basically adding seed data for user roles
+    /// </summary>
+    public class RolesConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+                new IdentityRole
+                {
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                }
+            );
+        }
+    }
+}
